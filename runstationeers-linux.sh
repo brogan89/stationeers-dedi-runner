2d4b41ea9749b5193577f38ca0c5832ed797c8c4
brogan89 <brogan.king@outlook.com>
2022-08-01 13:40:50 +1200

    Update runstationeers-linux.sh

--- runstationeers-linux.sh
+++ runstationeers-linux.sh
@@ -2,16 +2,10 @@
 
 # create place to store save/ scripts/ setting.xml etc...
 DATA_DIR="${PWD}/data"
-echo "DATA_DIR=${DATA_DIR}"
-
-# set home
-HOME="/home/brogan"
-echo "HOME=${HOME}"
 
 # set exe
 # EXECUTABLE="$HOME/.steam/debian-installation/steamapps/common/Stationeers Dedicated Server/rocketstation_DedicatedServer.x86_64"
-EXECUTABLE="$HOME/Unity/stationeers/Builds/LinuxServer/rocketstation.x86_64"
-echo "EXECUTABLE=${EXECUTABLE}"
+EXECUTABLE="/home/brogan/Unity/stationeers/Builds/LinuxServer/rocketstation.x86_64"
 
 # --- Run Stationeers --- #
 
@@ -31,14 +25,10 @@ SAVE_PATH="$DATA_DIR/saves"
 LOG_FILE="$DATA_DIR/log.txt"
 SETTINGS_PATH="$DATA_DIR/settings.xml"
 
-echo "SAVE_PATH=${SAVE_PATH}"
-echo "LOG_FILE=${LOG_FILE}"
-echo "SETTINGS_PATH=${SETTINGS_PATH}"
-
 # Run the server
 "$EXECUTABLE" \
 -loadlatest "$SAVE_NAME" $WORLD_TYPE \
-# -logFile "$LOG_FILE" \
+-logFile "$LOG_FILE" \
 -settingspath "$SETTINGS_PATH" \
 -settings StartLocalHost $START_LOCAL_HOST ServerVisible $SERVER_VISIBLE \
     GamePort $GAME_PORT UPNPEnabled $UPNP_ENABLED ServerName "$SERVER_NAME" \
